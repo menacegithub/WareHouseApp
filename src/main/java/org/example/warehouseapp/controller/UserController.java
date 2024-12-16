6fb8ef6f4a018013e4e424b070dbc1fa0586b693
menacegithub <davvitomadumarov@gmail.com>
2024-12-16 15:23:51 +0500

    changes

--- src/main/java/org/example/warehouseapp/controller/UserController.java
+++ src/main/java/org/example/warehouseapp/controller/UserController.java
@@ -1,14 +1,50 @@
 package org.example.warehouseapp.controller;
 
+import org.example.warehouseapp.model.Result;
+import org.example.warehouseapp.model.User;
 import org.example.warehouseapp.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
 @RestController()
 @RequestMapping("{/user}")
 public class UserController {
     @Autowired
     private UserService userService;
 
+    @GetMapping
+    public List<User> getUsers() {
+        List<User> allUsers = userService.getAllUsers();
+        return allUsers;
+    }
 
+    @GetMapping("/user/{id}")
+    public User getUserById(@PathVariable Integer id) {
+        return userService.getUserById(id);
+    }
+
+    @PostMapping
+    public Result createUser(@RequestBody User user) {
+        Result user1 = userService.createUser(user);
+        return user1;
+
+
+    }
+
+    @PutMapping
+    public Result updateUser(@RequestBody User user, @PathVariable Integer id) {
+        Result result = userService.updateUser(user, id);
+        return result;
+
+    }
+
+    @DeleteMapping
+    public Result deleteUser(@PathVariable Integer id) {
+        Result result = userService.deleteUser(id);
+        return result;
+
+
+    }
 }
--- src/main/java/org/example/warehouseapp/service/UserService.java
+++ src/main/java/org/example/warehouseapp/service/UserService.java
@@ -1,8 +1,54 @@
 package org.example.warehouseapp.service;
 
+import org.example.warehouseapp.model.Result;
+import org.example.warehouseapp.model.User;
+import org.example.warehouseapp.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
 
+    @Autowired
+    UserRepository userRepository;
+
+
+    public List<User> getAllUsers() {
+        List<User> userList = userRepository.findAll();
+        return userList;
+    }
+    public User getUserById(Integer id) {
+        Optional<User> userOptional = userRepository.findById(id);
+        return userOptional.get();
+    }
+    public Result createUser(User user) {
+        User user1 = new User();
+        user1.setFirstname(user.getFirstname());
+        user1.setLastname(user.getLastname());
+        user1.setPassword(user.getPassword());
+
+        userRepository.save(user1);
+        return new Result(true,"succes");
+    }
+    public Result updateUser(User user, Integer id) {
+        Optional<User> userOptional = userRepository.findById(user.getId());
+        if (userOptional.isPresent()) {
+
+            return new Result(true,"molodes");
+        }
+        return new Result(false,"user not found Mazgi");
+    }
+    public Result deleteUser(Integer id) {
+        Optional<User> userOptional = userRepository.findById(id);
+        if (userOptional.isPresent()) {
+            userRepository.delete(userOptional.get());
+            return new Result(true,"user deleted");
+        }
+        return new Result(false,"user not found Mazgi");
+    }
+
 }
+

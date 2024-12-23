478633292710772f572974cef88a964eb6db6cc3
menacegithub <davvitomadumarov@gmail.com>
2024-12-23 15:21:40 +0500

    add

--- src/main/java/org/example/warehouseapp/controller/CategoryController.java
+++ src/main/java/org/example/warehouseapp/controller/CategoryController.java
@@ -1,14 +1,21 @@
 package org.example.warehouseapp.controller;
 
+import org.example.warehouseapp.model.Category;
 import org.example.warehouseapp.service.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("{/categories}")
 public class CategoryController {
     @Autowired
     private CategoryService categoryService;
+
+    public List<Category> getAllCategories() {
+     return categoryService.categoriesAll();
+    }
 
 }
